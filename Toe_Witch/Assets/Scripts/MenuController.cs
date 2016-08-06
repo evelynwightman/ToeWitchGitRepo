312b95b2d5d1530bc8693cd81eae9ec570855579
evelynwightman <evelyn.wightman@gmail.com>
2016-08-06 16:35:48 -0300

    can theoretically close now

--- Toe_Witch/Assets/Scripts/MenuController.cs
+++ Toe_Witch/Assets/Scripts/MenuController.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.Events;
 
 public class MenuController : MonoBehaviour {
 
 	public GameObject menuPanel;
 	public GameObject menuToggleButton;
 	public float speed;
+	public GameObject yesNo;
 
 	private Vector3 onPosition = new Vector3(-200f, -70f, 0f);
-	private Vector3 offPosition = new Vector3(-760f, -70f, 0f);
+	private Vector3 offPosition = new Vector3(-760f, -120f, 0f);
 	private TextHandler textBlobs;
 
 	void Start(){
@@ -22,14 +24,12 @@ public class MenuController : MonoBehaviour {
 	}
 
 	public void ToggleMenu(){
-		Debug.Log ("Toggle");
 		if (transform.position == onPosition) {
 			menuPanel.SetActive (false);
 			StartCoroutine ("SlideOffMenu");
 
 		} else {
-			menuPanel.SetActive (true);
-
+			menuPanel.SetActive (true); 
 			StartCoroutine ("SlideOnMenu");
 		}
 	}
@@ -39,15 +39,28 @@ public class MenuController : MonoBehaviour {
 			transform.position = Vector3.MoveTowards (transform.position, onPosition, speed * Time.deltaTime);
 			yield return null;
 		}
+		//pause game while menu is on
+		Time.timeScale = 0.0f;
+
 		menuToggleButton.transform.GetComponentInChildren<Text> ().text = textBlobs.Get ("menuClose");
 	}
 
 	IEnumerator SlideOffMenu(){
-		Debug.Log ("SlideOffMenu");
+		//resume game
+		Time.timeScale = 1.0f;
+
 		while (!Mathf.Approximately (transform.position.magnitude, offPosition.magnitude)) {
 			transform.position = Vector3.MoveTowards (transform.position, offPosition, speed * Time.deltaTime);
 			yield return null;
 		}
 		menuToggleButton.transform.GetComponentInChildren<Text> ().text = textBlobs.Get ("menuOpen");
+	}
+
+	public void OfferQuit(){
+		yesNo.transform.GetComponent<YesNoHandler> ().PoseQuestion (textBlobs.Get ("quitQuery"), new UnityAction (QuitGame));
+	}
+
+	void QuitGame(){
+		Application.Quit();
 	}
 }
---
+++ Toe_Witch/Assets/Scripts/YesNoHandler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class YesNoHandler : MonoBehaviour {
+
+	public Text question;
+	public Button yesButton;
+	public Button noButton;
+
+	private TextHandler textBlob;
+
+	void Start(){
+		textBlob = GameObject.Find("GameManager").GetComponent<TextHandler> ();
+		if (textBlob == null) {
+			Debug.LogError ("Dialogue Displayer could not find TextHandler");
+		}
+
+		this.gameObject.SetActive (false);
+	}
+
+	void Choice(UnityAction yesEvent, UnityAction noEvent = null){
+		this.gameObject.SetActive (true);
+
+		yesButton.onClick.RemoveAllListeners ();
+		yesButton.onClick.AddListener (yesEvent);
+
+		noButton.onClick.RemoveAllListeners ();
+		noButton.onClick.AddListener (noEvent);
+	}
+
+	void SetUpText(string questionText, string yesText, string noText){
+		question.text = questionText;
+
+		this.yesButton.transform.GetComponentInChildren<Text> ().text = yesText;
+
+		this.noButton.transform.GetComponentInChildren<Text> ().text = noText;
+	}
+
+	public void PoseQuestion(string question, UnityAction yesAction){
+		SetUpText (question, textBlob.Get ("yes"), textBlob.Get ("no"));
+		Choice(yesAction, new UnityAction(NeverMind));
+	}
+
+	void NeverMind(){
+		this.gameObject.SetActive (false);
+	}
+}
